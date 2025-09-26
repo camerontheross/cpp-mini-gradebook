61900b2934e8ef69d67fc2c77695de64f56acbed
camerontheross <camerontheross@gmail.com>
2025-09-26 15:10:13 -0400

    create gradebook.cpp and outline needed functions

---
+++ gradebook.cpp
@@ -0,0 +1,25 @@
+/*  gradebook.cpp
+ *  Written By: Cameron Ross
+ *  Last Edit : 26 September 2025
+ *
+ *  Purpose :
+ *  Input   :
+ *  Process :
+ *  Output  :
+ */
+
+// addStudent()
+//
+// getStudentCount()
+//
+// getClassGradeAverage()
+//
+// getClassHigh()
+//
+// getClassLow()
+//
+// outputClassData()
+//
+// counterControlledAddStudents()
+//
+// sentinelControlledAddStudents()
