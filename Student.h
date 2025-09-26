5871fd7a780dcae8d5de8497e8fcb464efcce9fb
camerontheross <camerontheross@gmail.com>
2025-09-26 13:50:29 -0400

    create Student struct and tested inside main

---
+++ Student.h
@@ -0,0 +1,24 @@
+/*
+ *  Student.cpp
+ *  Written by  : Cameron Ross
+ *  Last edit   : 26 September 2025
+ *
+ *  Purpose : Define a student struct for use in main logic
+ *  Input   : None
+ *  Process : Declare variables for name(string)
+ *            and class grade(double)
+ *  Output  : None
+ */
+
+#ifndef STUDENT_H
+
+#include <string>
+using namespace std;
+
+struct Student
+{
+    string name;
+    double classGrade;
+};
+
+#endif // !STUDENT_H
---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ *  main.cpp
+ *  Written By  : Cameron Ross
+ *  Last edit   : 26 September 2025
+ */
+
+#include "Student.h"
+#include <iostream>
+
+int main()
+{
+    Student student;
+    student.name = "Bob";
+    cout << student.name << endl;
+    return 0;
+}
