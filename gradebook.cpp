568a498da29a2f341c4531c5eef91f928d8acccc
camerontheross <camerontheross@gmail.com>
2025-09-29 21:24:05 -0400

    impliment selectable menu with quit and input validation

--- gradebook.cpp
+++ gradebook.cpp
@@ -1,11 +1,7 @@
-/*  gradebook.cpp
+/*
+ * gradebook.cpp
  *  Written By: Cameron Ross
  *  Last Edit : 26 September 2025
- *
- *  Purpose :
- *  Input   :
- *  Process :
- *  Output  :
  */
 
 #include "Student.h"
@@ -13,9 +9,15 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+/*
+ *  Purpose :   output the main menu options
+ *  Input   :   none
+ *  Process :   cout menu options
+ *  Output  :   menu options
+ */
 void showMenu()
 {
+    // output the menu options to terminal
     cout << "Choose action from the following: \n"
          << "1. Enter a known number of students. (counter)" << "\n"
          << "2. Enter an unknown number of students. (sentinel)" << "\n"
@@ -23,7 +25,12 @@ void showMenu()
          << "4. Quit the program" << "\n"
          << "Enter your choice(1 - 4): ";
 }
-
+/*
+ *  Purpose :   Create and return a new student struct
+ *  Input   :   none
+ *  Process :   take input, validate and pass values to Student struct
+ *  Output  :   returns a student onbject
+ */
 Student createStudent()
 {
     double studentGrade; // double to store the student grade
@@ -44,28 +51,37 @@ Student createStudent()
 int main()
 {
     const int COUNTER_CONTROL_CHOICE = 1, SENTINEL_CONTROL_CHOICE = 2,
-              PRINT_CHOICE = 3, QUIT_CHOICE = 4;
+              PRINT_CHOICE = 3,
+              QUIT_CHOICE = 4; // integers to represent menu selections
 
-    int userChoice; // declare variable for user choice
+    bool mainLoopActive = true; // declare sentinel for main loop
+    int userChoice;             // declare variable for user choice
 
     vector<Student> userClass; // vector to store students
 
     do
     {
-        showMenu();
+        showMenu(); // call the method to print the menu
         cin >> userChoice;
 
         switch (userChoice)
         {
-        case COUNTER_CONTROL_CHOICE:
-        case SENTINEL_CONTROL_CHOICE:
-        case PRINT_CHOICE:
-        case QUIT_CHOICE:
-        default:
-            cout << "placeholder";
+        case COUNTER_CONTROL_CHOICE: // case for input = 1
+            break;
+        case SENTINEL_CONTROL_CHOICE: // case for input = 2
+            break;
+        case PRINT_CHOICE: // case for input = 3
+            break;
+        case QUIT_CHOICE: // case for input = 4
+            mainLoopActive = false;
+            break;
+        default:                              // case for unhandled inputs
+            cout << "Invalid choice entered." // output to user that input is
+                                              // invalid
+                 << "Please enter a valid selection within range (1 - 3)";
         }
 
-    } while (userChoice != QUIT_CHOICE);
+    } while (mainLoopActive);
 
     return 0;
 }
